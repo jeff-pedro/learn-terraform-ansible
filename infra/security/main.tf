f1c988bfc16d2b00d509c0b9ed688b6198a01a28
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2024-01-27 18:12:25 -0300

    adiciona recurso de grupo de segurança

---
+++ infra/security/main.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "general_access" {
+  name        = "general_access"
+  description = "development team group"
+  ingress {
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+  }
+  egress {
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+  }
+  tags = {
+    Name = "general_access"
+  }
+}
