db78137f0f41190f640d960371f0b77311046f39
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2024-01-30 13:02:54 -0300

    cria template da máquinas na aws

--- infra/main.tf
+++ infra/main.tf
@@ -13,18 +13,18 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_instance" "app_server" {
-  ami           = var.aws_image
+resource "aws_launch_template" "machine" {
+  image_id      = var.aws_image
   instance_type = var.aws_instance
   key_name      = var.key
-  
+
   tags = {
     Name = var.aws_tag_name
   }
 }
 
 resource "aws_key_pair" "aws_key" {
-  key_name =  var.key
+  key_name   = var.key
   public_key = file("${var.key}.pub")
 }
 
